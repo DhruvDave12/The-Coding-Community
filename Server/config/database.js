f623581f6a0b1d3d284e2a396660c2569167ef07
Dhruv Dave <78278281+DhruvDave12@users.noreply.github.com>
2022-03-26 23:00:21 +0530

    added cloud DB and Procfile

--- Server/config/database.js
+++ Server/config/database.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
-
+const URL = process.env.MONGO_CLOUD_URL || 'mongodb://localhost:27017/codingCommData';
 module.exports.configDB = () => {
     // Establishing MongoConnection
-    mongoose.connect('mongodb://localhost:27017/codingCommData');
+    mongoose.connect(URL);
     const db = mongoose.connection;
     db.on("error", console.error.bind(console, "Connection Error: "));
     db.once("open", () => {
         console.log("DATABASE CONNECTED");
     });
 }
+
